96ab8339792aba6af0be564a307152c9108dbb14
sitabra <sitabra.das@cbnits.com>
2022-03-25 18:34:28 +0530

    dynamic chahnges

--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,9 +8,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<int> top = <int>[];
-  List<int> bottom = <int>[0];
   final _pageController = PageController(viewportFraction: 0.87);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,20 +21,20 @@ class _HomePageState extends State<HomePage> {
       ),
       body: SafeArea(
           child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                greeting(),
-                categoryItems(context),
-                lessonsText(),
-                listView(context),
-                challenges(),
-                //listViewVertical(context),
-                challengeItems(context),
-                tryItems(context),
-              ],
-            ),
-          )),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children:  [
+            greeting(),
+            categoryItems(context),
+
+            lessonsText(),
+            //listView(context),
+           // challenges(),
+           // challengeItems(context),
+           // tryItems(context),
+          ],
+        ),
+      )),
     );
   }
 
@@ -47,13 +44,16 @@ class _HomePageState extends State<HomePage> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: const [
-          Text('Hi John',
-            style: TextStyle(fontSize: 32),),
-          Text('You have completed\n6 lessons this week',
-            style: TextStyle(fontSize: 14),),
+          Text(
+            'Hi John',
+            style: TextStyle(fontSize: 32),
+          ),
+          Text(
+            'You have completed\n6 lessons this week',
+            style: TextStyle(fontSize: 14),
+          ),
         ],
       ),
-
     );
   }
 
@@ -71,8 +71,7 @@ class _HomePageState extends State<HomePage> {
               color: Colors.white60,
               child: Center(child: Text('$index')),
             );
-          }
-      ),
+          }),
     );
   }
 
@@ -81,12 +80,11 @@ class _HomePageState extends State<HomePage> {
       margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
-          Text('Today\'s lessons',
-            style: TextStyle(fontSize: 15),),
+        children:  [
+          const Text('Today\'s lessons', style: TextStyle(fontSize: 15),),
+         // ListView.builder(itemBuilder: itemBuilder)
         ],
       ),
-
     );
   }
 
@@ -102,24 +100,12 @@ class _HomePageState extends State<HomePage> {
               onTap: () {},
               child: Container(
                 width: 250,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Colors.redAccent),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.redAccent),
                 child: const Center(
-                  child: Text('List View 1',
-                  style: TextStyle(fontSize: 20, color: Colors.white60),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: InkWell(
-              onTap: () {},
-              child: Container(
-                width: 250,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Colors.orangeAccent),
-                child: const Center(
-                  child: Text('List View 2',
+                  child: Text(
+                    'List View 1',
                     style: TextStyle(fontSize: 20, color: Colors.white60),
                   ),
                 ),
@@ -132,9 +118,12 @@ class _HomePageState extends State<HomePage> {
               onTap: () {},
               child: Container(
                 width: 250,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Colors.greenAccent),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.orangeAccent),
                 child: const Center(
-                  child: Text('List View 3',
+                  child: Text(
+                    'List View 2',
                     style: TextStyle(fontSize: 20, color: Colors.white60),
                   ),
                 ),
@@ -147,9 +136,12 @@ class _HomePageState extends State<HomePage> {
               onTap: () {},
               child: Container(
                 width: 250,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Colors.yellowAccent),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.greenAccent),
                 child: const Center(
-                  child: Text('List View 4',
+                  child: Text(
+                    'List View 3',
                     style: TextStyle(fontSize: 20, color: Colors.white60),
                   ),
                 ),
@@ -162,9 +154,30 @@ class _HomePageState extends State<HomePage> {
               onTap: () {},
               child: Container(
                 width: 250,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Colors.blueAccent),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.yellowAccent),
                 child: const Center(
-                  child: Text('List View 5',
+                  child: Text(
+                    'List View 4',
+                    style: TextStyle(fontSize: 20, color: Colors.white60),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: InkWell(
+              onTap: () {},
+              child: Container(
+                width: 250,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.blueAccent),
+                child: const Center(
+                  child: Text(
+                    'List View 5',
                     style: TextStyle(fontSize: 20, color: Colors.white60),
                   ),
                 ),
@@ -184,73 +197,32 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: const [
-            Text('Challenges',
-              style: TextStyle(fontSize: 15),),
+            Text(
+              'Challenges',
+              style: TextStyle(fontSize: 15),
+            ),
           ],
         ),
-
       ),
     );
   }
 
-  // Widget listViewVertical(BuildContext context) {
-  //   const Key centerKey = ValueKey<String>('bottom-sliver-list');
-  //   return Scaffold(
-  //     body: CustomScrollView(
-  //       center: centerKey,
-  //       slivers: <Widget>[
-  //         SliverList(
-  //           delegate: SliverChildBuilderDelegate(
-  //                 (BuildContext context, int index) {
-  //               return Container(
-  //                 alignment: Alignment.center,
-  //                 color: Colors.blue[200 + top[index] % 4 * 100],
-  //                 height: 100 + top[index] % 4 * 20.0,
-  //                 child: Text('Item: ${top[index]}'),
-  //               );
-  //             },
-  //             childCount: top.length,
-  //           ),
-  //         ),
-  //         SliverList(
-  //           key: centerKey,
-  //           delegate: SliverChildBuilderDelegate(
-  //                 (BuildContext context, int index) {
-  //               return Container(
-  //                 alignment: Alignment.center,
-  //                 color: Colors.blue[200 + bottom[index] % 4 * 100],
-  //                 height: 100 + bottom[index] % 4 * 20.0,
-  //                 child: Text('Item: ${bottom[index]}'),
-  //               );
-  //             },
-  //             childCount: bottom.length,
-  //           ),
-  //         ),
-  //       ],
-  //     ),
-  //   );
-  // }
-}
-
   Widget challengeItems(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(15.0),
-      child: Container(
-        width: double.infinity,
-        height: 95,
-        child: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 1,
-            ),
-            itemCount: 4,
-            itemBuilder: (BuildContext context, int index) {
-              return Card(
-                color: Colors.grey,
-                child: Center(child: Text('$index')),
-              );
-            }
-        ),
-      ),
+      child: Scaffold(
+          body: ListView.separated(
+              separatorBuilder: (context, index) {
+                return const SizedBox(height: 5);
+              },
+              itemBuilder: (context, index) {
+                return Container(
+                  height: 10,
+                  color: Colors.blue,
+                  child: Text('Item $index'),
+                );
+              },
+              itemCount: 8)),
     );
   }
 
@@ -258,22 +230,20 @@ class _HomePageState extends State<HomePage> {
     return Padding(
       padding: const EdgeInsets.all(15.0),
       child: Container(
-        height: 100,
-        width: double.infinity,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: 50,
-            itemBuilder: (BuildContext context, int index) {
-              return Container(
-                margin: EdgeInsets.symmetric(horizontal: 2),
-                height: 50,
-                width: 50,
-                color: Colors.grey,
-                child: Center(child: Text('$index' + 'A')),
-              );
-            }
-        )
-      ),
+          height: 100,
+          width: double.infinity,
+          child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 50,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  margin: EdgeInsets.symmetric(horizontal: 2),
+                  height: 50,
+                  width: 50,
+                  color: Colors.grey,
+                  child: Center(child: Text('$index' + 'A')),
+                );
+              })),
     );
   }
-
+}
